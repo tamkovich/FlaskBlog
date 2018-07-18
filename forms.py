3909351328590b8447cc471510b2e812116e8630
Denis Tamkovich <jaselnikpromise@gmail.com>
2018-07-18 20:32:04 +0300

    Login|Logout|Register with DB+session

--- forms.py
+++ forms.py
@@ -1,20 +1,18 @@
 from wtforms import (
     Form,
     validators,
-    TextField,
+    StringField,
     PasswordField,
     BooleanField,
 )
 
 
 class RegistrationForm(Form):
-    username = TextField('Username', [validators.Length(min=4, max=20)])
-    email = TextField('Email Adress', [validators.Length(min=4, max=20)])
-    password = PasswordField('Password', [validators.Required(),
-                                          validators.EqualTo('confirm', message='Password must mutch')])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
     confirm = PasswordField('Repeat Password')
-
-    accept_tos = BooleanField('I accept the Terms of Service and the Privacy Notice '
-                              '(Last upadate Jul 17 2018', [validators.Required()])
-
-
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
